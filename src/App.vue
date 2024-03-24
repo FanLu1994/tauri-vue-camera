cda898161e940fd9614d6b427df3a29b9f393086
xiamu <1066580119@qq.com>
2024-03-24 13:55:29 +0800

    update: base commit

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,47 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import { useDevicesList } from '@vueuse/core'
+import {onBeforeMount, onMounted, ref} from "vue";
+
+const videoRef = ref(null)
+
+onBeforeMount(async () => {
+  await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+})
+
+
+
+const {
+  devices,
+  videoInputs: cameras,
+  audioInputs: microphones,
+  audioOutputs: speakers,
+} = useDevicesList()
+
+onMounted(()=>{
+
+})
+
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
+  <div class="container" data-tauri-drag-region>
+    <video ref="videoRef" class="video"></video>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+    <div class="settings">
+      <div>
+        <select>
+          <option v-for="camera in cameras" :key="camera.deviceId" :value="camera.deviceId">
+            {{ camera.label }}
+          </option>
+        </select>
+      </div>
+
     </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
   </div>
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
 </style>
