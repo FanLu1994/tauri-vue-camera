e6dfc2fa41e89fc69e838f4932d750f6ab4ae248
xiamu <1066580119@qq.com>
2024-04-05 16:16:18 +0800

    feat: 窗口复制功能，待完善

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,23 @@
 import {onBeforeMount, onMounted, ref, watchEffect} from 'vue'
 import { useDevicesList, useUserMedia } from '@vueuse/core'
 import HoverSelect from "./components/HoverSelect.vue";
-import {CameraOne,Round,RectangleOne,FlipHorizontally,Power} from "@icon-park/vue-next";
+import {CameraOne,Round,RectangleOne,FlipHorizontally,Power,Copy} from "@icon-park/vue-next";
 import { invoke } from '@tauri-apps/api/tauri'
+import {WebviewWindow} from "@tauri-apps/api/window";
+
+
+const second = ref("")
+onMounted(()=>{
+  // 获取当前页面的 URL
+  const url = window.location.href;
+  console.log(url)
+  // 创建 URLSearchParams 对象，用于处理 URL 查询参数
+  const params = new URLSearchParams(new URL(url).search);
+  // 获取特定参数的值
+  const paramValue = params.get('second');
+  console.log('参数值:', paramValue);
+  second.value = paramValue
+})
 
 onBeforeMount(async () => {
   await navigator.mediaDevices.getUserMedia({
@@ -64,12 +79,39 @@ const onClose = async () => {
   await invoke('close', {})
 }
 
+// 打开新窗口
+const onOpen = async () => {
+  let win = new WebviewWindow("Second",
+      {
+        label: 'Second',
+        title: '第二个窗口',
+        url: '/?second=1',
+        width: 300,
+        height: 200,
+        resizable: true,
+        alwaysOnTop: false,
+        transparent:true,
+        hiddenTitle:true,
+        decorations:false,
+      }
+  )
+
+  // 窗口创建完毕/失败
+  win.once('tauri://created', async() => {
+    console.log('window create success!')
+  })
+
+  win.once('tauri://error', async() => {
+    console.log('window create error!')
+  })
+}
+
 </script>
 
 <template>
   <div class="container" data-tauri-drag-region>
     <power theme="outline" size="24" fill="#ef4444" class="close-btn" @click="onClose"/>
-
+    {{second}}
 <!--    <video ref="videoRef" class="video"></video>-->
     <video ref="videoRef" muted autoplay class="h-100 w-auto video"
            :class="{'circle':shape==='circle','rectangle':shape==='rectangle','mirror':mirror?'mirror':''}"
@@ -77,8 +119,8 @@ const onClose = async () => {
 
     <div class="settings">
       <div @click="onChangeShape" title="形状切换">
-        <round v-show="shape==='rectangle'" theme="outline" size="24" fill="#ea580c"/>
-        <rectangle-one v-show="shape==='circle'" theme="outline" size="24" fill="#ea580c"/>
+        <round v-show="shape==='rectangle'" theme="outline" size="24" fill="#334155"/>
+        <rectangle-one v-show="shape==='circle'" theme="outline" size="24" fill="#334155"/>
       </div>
 
       <div>
@@ -89,15 +131,18 @@ const onClose = async () => {
 <!--        </select>-->
         <hover-select :options="cameras" @change="onCameraChange" label="label" value="deviceId" >
           <template #icon>
-            <camera-one theme="outline" size="24" fill="blue"/>
+            <camera-one theme="outline" size="24" fill="#334155"/>
           </template>
         </hover-select>
       </div>
 
       <div @click="mirror=!mirror" title="镜像">
-        <flip-horizontally theme="outline" size="24" fill="#9333ea"/>
+        <flip-horizontally theme="outline" size="24" fill="#334155"/>
       </div>
 
+      <div @click="onOpen" title="新开窗口">
+        <copy theme="outline" size="24" fill="#334155"/>
+      </div>
 
     </div>
   </div>
